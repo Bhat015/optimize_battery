759dcf21a38377df7d0d6747c587f7f7ed68b58b
Bhat015 <ganeshbhat015@gmail.com>
2022-03-21 07:22:45 +0530

    Update changelog and add API documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,16 @@
+## 0.0.3
+
+Upgrade to dart 2.16
+
+Support Android 31
+
+Fix reply already submitted issue [issues-2](https://github.com/Bhat015/optimize_battery/issues/2)
+
+Add API documentation
+
 ## 0.0.2
 
 Fix auto generated plugin registrant issue [issues-1](https://github.com/Bhat015/optimize_battery/issues/1)
-Add documentation
-
 
 ## 0.0.1
 
--- lib/optimize_battery.dart
+++ lib/optimize_battery.dart
@@ -4,21 +4,38 @@ import 'package:flutter/services.dart';
 class OptimizeBattery {
   static const MethodChannel _channel = MethodChannel('optimize_battery');
 
-  // check if the app ignoring BatteryOptimizations
+  /// checks if the app is ignoring BatteryOptimization.
+  /// 
+  /// returns true if the app is ignoring BatteryOptimization.
+  /// returns false if the app is not ignoring BatteryOptimization.
   static Future<bool> isIgnoringBatteryOptimizations() async {
     final reading =
         await _channel.invokeMethod('isIgnoringBatteryOptimizations');
     return reading!;
   }
 
-  // open BatteryOptimizations setting page
+  /// opens BatteryOptimizations setting page
+  /// Lets the user to navigate to the app and enable/disable BatteryOptimizations.
+  /// 
+  /// returns true if the settings page is opened
   static Future<bool> openBatteryOptimizationSettings() async {
     final reading =
         await _channel.invokeMethod('openBatteryOptimizationSettings');
     return reading!;
   }
 
-  // stop battery optimization
+  /// stops battery optimization
+  /// 
+  /// To use this method you need to have the following permissions in mainfest file:
+  /// 
+  /// `<uses-permission  android:name="android.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS"/>`
+  /// 
+  /// WARNING: It uses the intent(ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS). 
+  /// Which is not recommended unless its an excemtion case. 
+  /// 
+  /// Read more here : https://developer.android.com/training/monitoring-device-state/doze-standby#exemption-cases
+  /// 
+  /// returns true if stop optimization dialog is shown
   static Future<bool> stopOptimizingBatteryUsage() async {
     final reading = await _channel.invokeMethod('stopOptimizingBatteryUsage');
     return reading!;
